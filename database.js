1efa5b99bc3ae6857cc59675a9df0c09cb4a1848
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-10 16:23:01 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,8 @@
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://website:webwebweb@tutorcenter.rdnpr1a.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("please");
+  // perform actions on the collection object
+  client.close();
+});
