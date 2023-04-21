224c40cdf10a5d8f9f55a723ab6aa04ee2a3ae86
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-21 17:50:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post("/login", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
 // route for handling login requests
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
