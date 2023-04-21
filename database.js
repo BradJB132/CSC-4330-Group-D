ca756c67e2c428c78512328d0d7415578ae68c67
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-21 17:19:45 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://website:webwebweb@tutorcenter.rdnpr1a.mongodb.net/UserAccounts";
+// connect to the database
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
+// define the schema for a user
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// define the User model using the user schema
+const User = mongoose.model('User', userSchema);
+
+// export the Mongoose connection and models
+module.exports = {
+  connection: mongoose.connection,
+  User: User
+};
