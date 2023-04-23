81d421b9837a14c85e5c6b00791834f7ef9503e1
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-23 13:56:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/logout', function(req, res) {
 app.get('/account', async (req, res) => {
   try {
     // find the user by username
-    const user = await User.findOne({ username: req.body.username }).exec();
+    const user = await User.findOne({ username: req.body.username, firstName: req.body.firstName, lastName: req.body.lastName }).exec();
     if (user) {
       // render the Account.html template with the user data
       res.render('Account', { user });
