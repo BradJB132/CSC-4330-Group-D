6ebcee0f83c65ec9566633e3b1acf3fd4e1b45dd
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-23 19:52:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.get('/account', async (req, res) => {
        if (err) throw err;
        const firstName = user.firstName;
        const lastName = user.lastName;
-       const email = user.username;
-       res.render('Account', { firstName, lastName, email });
+       //const email = user.username;
+       res.render('Account', { firstName, lastName });
     });
     //res.sendFile(path.join(__dirname, 'html', 'Account.html'));
 });
