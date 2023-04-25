8b9a3d313db6004dbea3a223f9105ad79b8e4242
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-25 12:33:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/homepage', async (req, res) => {
     const firstName = user.firstName;
     const lastName = user.lastName;
     const email = user.email;
-    const subjects = user.subjects;
+    const subjects = tutors.subjects;
     const role = user.role;
     if(role == "Admin")
       res.redirect('/admin');
