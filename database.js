edcc13c1d994d8559c802a882234424552a0bf23
cnhayes24 <93173774+cnhayes24@users.noreply.github.com>
2023-04-24 23:40:04 -0500

    Update database.js

--- database.js
+++ database.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: true,
-    enum: ['Student', 'Tutor', 'Acadmin']
+    enum: ['Student', 'Tutor', 'Admin']
   }
 });
 
